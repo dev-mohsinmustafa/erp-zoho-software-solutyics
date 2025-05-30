3c92a23574e9062cd814ff13c3d45e7ef7540ccf
Your GitHub Username <mohsinmustafaansari@gmail.com>
2025-05-30 11:25:15 +0500

    set loading in tax table.

--- app/(back-office)/dashboard/banking/taxes/page.jsx
+++ app/(back-office)/dashboard/banking/taxes/page.jsx
@@ -1,18 +1,40 @@
+"use client";
 import DataTableTaxes from "@/components/dashboard/DataTableTaxes";
 import FixedHeader from "@/components/dashboard/FixedHeader";
+import LoadingSpinner from "@/components/dashboard/LoadingSpinner";
 import { getData } from "@/lib/getData";
+import { useEffect, useState } from "react";
 
-const Taxes = async () => {
-    const taxes = await getData("banking/taxes");
+const Taxes = () => {
+    // const taxes = await getData("banking/taxes");
     const columns = ["title", "rate", "type", "description", "isDefault", "createdAt", "updatedAt"];
 
+    const [taxes, setTaxes] = useState([]);
+    const [loading, setLoading] = useState(false);
+    async function fetchData() {
+        setLoading(true);
+        try {
+            const data = await getData("banking/taxes");
+            setTaxes(data);
+        } catch (error) {
+            console.error("Error fetching taxes:", error);
+        }
+        finally {
+            setLoading(false);
+        }
+    }
+    useEffect(() => {
+
+        fetchData();
+    }, []);
     return (
         <div>
             {/* Fixed Header */}
             <FixedHeader title="Taxes" newLink="/dashboard/banking/taxes/new" />
 
             {/* Table */}
-            <div className="my-4 p-8">
+            <div className="my-4 p-8 relative">
+                {loading ? <LoadingSpinner message="Loading taxes data, please wait..." /> : null}
                 <DataTableTaxes data={taxes} columns={columns} resourceTitle="banking/taxes" />
             </div>
         </div>
