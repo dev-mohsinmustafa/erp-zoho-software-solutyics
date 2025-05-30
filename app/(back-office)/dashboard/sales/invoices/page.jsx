35e0693ff7d84c9870e8aac5aa1eee798c8ecf08
Your GitHub Username <mohsinmustafaansari@gmail.com>
2025-05-30 11:28:54 +0500

    set loading in invoices table.

--- app/(back-office)/dashboard/sales/invoices/page.jsx
+++ app/(back-office)/dashboard/sales/invoices/page.jsx
@@ -1,17 +1,38 @@
+"use client";
 import DataTableInvoices from "@/components/dashboard/DataTableInvoices";
 import FixedHeader from "@/components/dashboard/FixedHeader";
+import LoadingSpinner from "@/components/dashboard/LoadingSpinner";
 import { getData } from "@/lib/getData";
+import { useEffect, useState } from "react";
 
-const CustomerInvoices = async () => {
-    const invoices = await getData("sales/invoices");
+const CustomerInvoices = () => {
+    // const invoices = await getData("sales/invoices");
     const columns = ["invoiceNumber", "invoiceDate", "dueDate", "status", "customer.name", "total"];
+
+    const [invoices, setInvoices] = useState([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const data = await getData("sales/invoices");
+                setInvoices(data);
+            } catch (error) {
+                console.error("Error fetching invoices:", error);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
         <div>
             {/* Fixed Header */}
             <FixedHeader title="Customer Invoices" newLink="/dashboard/sales/invoices/new" />
             {/* Table */}
-            <div className="my-4 p-8">
+            <div className="my-4 p-8 relative">
+                {loading ? <LoadingSpinner message="Loading invoices data, please wait..." /> : null}
                 <DataTableInvoices
                     data={invoices}
                     columns={columns}
